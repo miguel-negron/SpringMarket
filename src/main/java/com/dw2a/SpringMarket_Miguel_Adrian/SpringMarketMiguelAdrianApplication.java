697f9ee86630fb98d54bec4a712e6c8af9a68762
miguel <miguel.negron@gmail.com>
2020-11-05 21:28:33 +0100

    creacion del proyecto, copiar pom y properties, copiar plantillas

---
+++ src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/SpringMarketMiguelAdrianApplication.java
@@ -0,0 +1,13 @@
+package com.dw2a.SpringMarket_Miguel_Adrian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMarketMiguelAdrianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMarketMiguelAdrianApplication.class, args);
+	}
+
+}
