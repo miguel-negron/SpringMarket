f34724580fa0eacce0bdc8c44c5a537225d0058c
miguel <miguel.negron@gmail.com>
2020-11-08 20:03:20 +0100

    Producto.java rellenada

--- src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/entidades/Producto.java
+++ src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/entidades/Producto.java
@@ -1,8 +1,132 @@
 package com.dw2a.SpringMarket_Miguel_Adrian.entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Producto {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String nombre;
+	private Double precio;
+	private String descripcion;
+	private Integer unidades;
+
+	public Producto() {
+		super();
+	}
+	
+	public Producto(String nombre, Double precio, String descripcion) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+		this.descripcion = descripcion;
+		this.unidades = 0;
+	}
+
+	public Producto(String nombre, Double precio, String descripcion, Integer unidades) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+		this.descripcion = descripcion;
+		this.unidades = unidades;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Integer getUnidades() {
+		return unidades;
+	}
+
+	public void setUnidades(Integer unidades) {
+		this.unidades = unidades;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((precio == null) ? 0 : precio.hashCode());
+		result = prime * result + ((unidades == null) ? 0 : unidades.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Producto other = (Producto) obj;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (precio == null) {
+			if (other.precio != null)
+				return false;
+		} else if (!precio.equals(other.precio))
+			return false;
+		if (unidades == null) {
+			if (other.unidades != null)
+				return false;
+		} else if (!unidades.equals(other.unidades))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", descripcion=" + descripcion
+				+ ", unidades=" + unidades + "]";
+	}
 }
