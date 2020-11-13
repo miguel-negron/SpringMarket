e6c6a9c5704fbea362d80621653c080bfb0d3fe7
Adrian-Rubio <adrian41198@gmail.com>
2020-11-13 14:43:01 +0100

    controller index

--- src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/controladores/IndexController.java
+++ src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/controladores/IndexController.java
@@ -1,14 +1,21 @@
 package com.dw2a.SpringMarket_Miguel_Adrian.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.dw2a.SpringMarket_Miguel_Adrian.servicios.ProductoService;
+
 @Controller
 public class IndexController {
+	@Autowired
+	ProductoService productoService;
+
 	@GetMapping("/")
 	public String controller1(Model model) {
 
+		model.addAttribute("productos", productoService.listarProductos());
 		return "index";
 	}
 
