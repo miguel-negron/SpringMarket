58c08f31f6b768778e7a913e1f69a29c72002c92
Adrian-Rubio <adrian41198@gmail.com>
2020-11-13 13:00:42 +0100

    form producto

--- src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/controladores/IndexController.java
+++ src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/controladores/IndexController.java
@@ -1,8 +1,22 @@
 package com.dw2a.SpringMarket_Miguel_Adrian.controladores;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+	@GetMapping("/")
+	public String controller1(Model model) {
 
+		return "index";
+	}
+
+	@RequestMapping("/producto/crear")
+	public String crearProducto(Model model) {
+		return "crearProducto";
+
+
+	}
 }
