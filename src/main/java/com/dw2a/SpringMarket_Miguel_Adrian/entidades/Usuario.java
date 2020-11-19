68854d0ee506f88796bbe20d7603edd801d799dd
Adrian-Rubio <adrian41198@gmail.com>
2020-11-19 11:40:49 +0100

    Funcionalidades

--- src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/entidades/Usuario.java
+++ src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/entidades/Usuario.java
@@ -17,28 +17,27 @@ public class Usuario implements Serializable {
 
 	@Id
 	@GeneratedValue
-	
-	//DATOS PERSONALES DEL USUARIO
-	
+
+	// DATOS PERSONALES DEL USUARIO
+
 	private Long id;
 	private String nombre;
 	private String apellidos;
 	private String password;
 	private String email;
 	private String fechaNac;
-	
-	
-	//DATOS DE PAGO
-	
+
+	// DATOS DE PAGO
+
 	private Integer numeroTarjeta;
 	private Integer codigoSeguridad;
 	private String direccion;
-	
-	
+
 	public Usuario() {
 		super();
 	}
-	public Usuario(String nombre,String apellidos, String password, String email, String fechaNac,
+
+	public Usuario(String nombre, String apellidos, String password, String email, String fechaNac,
 			Integer numeroTarjeta, Integer codigoSeguridad, String direccion) {
 		super();
 		this.nombre = nombre;
@@ -47,9 +46,164 @@ public class Usuario implements Serializable {
 		this.email = email;
 		this.fechaNac = fechaNac;
 		this.numeroTarjeta = numeroTarjeta;
-		this.codigoSeguridad  = codigoSeguridad;
+		this.codigoSeguridad = codigoSeguridad;
 		this.direccion = direccion;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellidos == null) ? 0 : apellidos.hashCode());
+		result = prime * result + ((codigoSeguridad == null) ? 0 : codigoSeguridad.hashCode());
+		result = prime * result + ((direccion == null) ? 0 : direccion.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((fechaNac == null) ? 0 : fechaNac.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((numeroTarjeta == null) ? 0 : numeroTarjeta.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (apellidos == null) {
+			if (other.apellidos != null)
+				return false;
+		} else if (!apellidos.equals(other.apellidos))
+			return false;
+		if (codigoSeguridad == null) {
+			if (other.codigoSeguridad != null)
+				return false;
+		} else if (!codigoSeguridad.equals(other.codigoSeguridad))
+			return false;
+		if (direccion == null) {
+			if (other.direccion != null)
+				return false;
+		} else if (!direccion.equals(other.direccion))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (fechaNac == null) {
+			if (other.fechaNac != null)
+				return false;
+		} else if (!fechaNac.equals(other.fechaNac))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (numeroTarjeta == null) {
+			if (other.numeroTarjeta != null)
+				return false;
+		} else if (!numeroTarjeta.equals(other.numeroTarjeta))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFechaNac() {
+		return fechaNac;
+	}
+
+	public void setFechaNac(String fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+
+	public Integer getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+
+	public void setNumeroTarjeta(Integer numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+
+	public Integer getCodigoSeguridad() {
+		return codigoSeguridad;
+	}
+
+	public void setCodigoSeguridad(Integer codigoSeguridad) {
+		this.codigoSeguridad = codigoSeguridad;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
 	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", password=" + password
+				+ ", email=" + email + ", fechaNac=" + fechaNac + ", numeroTarjeta=" + numeroTarjeta
+				+ ", codigoSeguridad=" + codigoSeguridad + ", direccion=" + direccion + "]";
+	}
 
 }
