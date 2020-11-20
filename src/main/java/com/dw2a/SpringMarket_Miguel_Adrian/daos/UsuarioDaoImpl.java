637b82636e1a776ef91c9f3a5dfe54a728e1f261
miguel <miguel.negron@gmail.com>
2020-11-20 10:51:39 +0100

    user dao ahora es abstract

--- src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/daos/UsuarioDaoImpl.java
+++ src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/daos/UsuarioDaoImpl.java
@@ -1,5 +1,8 @@
 package com.dw2a.SpringMarket_Miguel_Adrian.daos;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +10,9 @@ import com.dw2a.SpringMarket_Miguel_Adrian.entidades.Usuario;
 
 @Repository
 @Component("UsuarioDao")
-public class UsuarioDaoImpl extends DaoGenericoImpl<Usuario> implements UsuarioDao {
+public abstract class UsuarioDaoImpl extends DaoGenericoImpl<Usuario> implements UsuarioDao {
+
+	@PersistenceContext
+	protected EntityManager em;
 
 }
