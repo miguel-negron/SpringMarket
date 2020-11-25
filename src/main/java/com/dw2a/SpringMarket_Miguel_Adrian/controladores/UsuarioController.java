18fd5848358afdca9ea626ad815f9879d2d4b1f0
miguel <miguel.negron@gmail.com>
2020-11-25 20:44:46 +0100

    avances en carrito

--- src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/controladores/UsuarioController.java
+++ src/main/java/com/dw2a/SpringMarket_Miguel_Adrian/controladores/UsuarioController.java
@@ -1,5 +1,11 @@
 package com.dw2a.SpringMarket_Miguel_Adrian.controladores;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,14 +32,14 @@ public class UsuarioController {
 	public String signUpPost(Model model, @RequestParam String nombre, @RequestParam String apellidos,
 			@RequestParam String password, @RequestParam String email, String fechaNac) {
 
-		if (usuarioService.obtenerUsuarioWhereNombre(nombre) != null) {
-			return "/usuario/signup";
-		} else {
+//		if (usuarioService.obtenerUsuarioWhereNombre(nombre) != null) {
+//			return "/usuario/signup";
+//		} else {
 			Usuario usuario = new Usuario(nombre, apellidos, password, email, fechaNac);
 			usuarioService.crearUsuario(usuario);
 
 			return "redirect:/usuario/perfil/" + usuario.getId();
-		}
+//		}
 	}
 
 	@GetMapping("/usuario/login")
@@ -42,12 +48,12 @@ public class UsuarioController {
 	}
 
 	@PostMapping("/usuario/login")
-	public String loginPost(Model model, @RequestParam String nombre, @RequestParam String password) {
+	public String loginPost(Model model, HttpSession session, @RequestParam String nombre, @RequestParam String password) {
 
 		Usuario usuario = usuarioService.obtenerUsuarioWhereNombre(nombre);
-		System.out.println(usuario.getPassword() + "  -  " + password);
+		
 		if (usuario.getPassword().equals(password)) {
-			System.out.println("Estamos dentro");
+			session.setAttribute("NOMBRE_USUARIO", usuario);
 		}
 
 		return "/usuario/login";
@@ -76,4 +82,31 @@ public class UsuarioController {
 		return "";
 	}
 
+	/*
+	 * 
+	 * Session cosas
+	 * 
+	 */
+
+	public String process(Model model, HttpSession session) {
+		@SuppressWarnings("unchecked")
+		Usuario usuario = (Usuario) session.getAttribute("NOMBRE_USUARIO");
+		if (usuario == null) {
+			usuario = new Usuario();
+		}
+		model.addAttribute("sessionMessages", usuario);
+		return "session";
+	}
+
+	public String persistMessage(@RequestParam("msg") String msg, HttpServletRequest request) {
+		@SuppressWarnings("unchecked")
+		List<String> messages= (List<String>) request.getSession().getAttribute("NOMBRE_USUARIO");
+		if(messages== null) {
+			messages= new ArrayList<>();
+			request.getSession().setAttribute("NOMBRE_USUARIO", messages);
+	}
+		messages.add(msg);
+	request.getSession().setAttribute("NOMBRE_USUARIO", messages);
+	return "redirect:/session";
+	}
 }
